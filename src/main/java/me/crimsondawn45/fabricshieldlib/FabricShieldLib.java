35c5438f107cc1356505f4a1784d9ce4a382bcea
Draylar <samuelhimes@gmail.com>
2020-09-06 14:55:43 -0500

    add dev check to test items/events/enchantments

--- src/main/java/me/crimsondawn45/fabricshieldlib/FabricShieldLib.java
+++ src/main/java/me/crimsondawn45/fabricshieldlib/FabricShieldLib.java
@@ -1,5 +1,6 @@
 package me.crimsondawn45.fabricshieldlib;
 
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -27,9 +28,12 @@ public class FabricShieldLib implements ModInitializer
 		ShieldRegistry.register(Items.SHIELD);
 		logger.info("Fabric Shield Lib Successfully Initialized!");
 
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "test_shield"), new BasicShield(new Item.Settings(), 100, 377, ItemTags.PLANKS));
-		Registry.register(Registry.ENCHANTMENT, new Identifier(MOD_ID, "test_enchantment"), new ShieldEnchantment(Rarity.COMMON, new TestEvent()));
+		// Only load test shield, test enchantment, and test event in development environments
+		if(FabricLoader.getInstance().isDevelopmentEnvironment()) {
+			Registry.register(Registry.ITEM, new Identifier(MOD_ID, "test_shield"), new BasicShield(new Item.Settings(), 100, 377, ItemTags.PLANKS));
+			Registry.register(Registry.ENCHANTMENT, new Identifier(MOD_ID, "test_enchantment"), new ShieldEnchantment(Rarity.COMMON, new TestEvent()));
 
-		ShieldRegistry.registerItemEvent(Items.SHIELD, new TestEvent());
+			ShieldRegistry.registerItemEvent(Items.SHIELD, new TestEvent());
+		}
 	}
 }
