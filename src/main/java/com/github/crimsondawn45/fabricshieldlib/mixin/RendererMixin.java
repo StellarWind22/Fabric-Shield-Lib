9eea0215d7cfd83e44b71e0f84a798d9c5a668b1
CringeStar <gozzook@gmail.com>
2021-10-17 13:43:33 -0700

    small critical changes

--- src/main/java/com/github/crimsondawn45/fabricshieldlib/mixin/RendererMixin.java
+++ src/main/java/com/github/crimsondawn45/fabricshieldlib/mixin/RendererMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.model.EntityModelLoader;
 import net.minecraft.client.render.entity.model.ShieldEntityModel;
 import net.minecraft.client.render.item.BuiltinModelItemRenderer;
+import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
@@ -44,7 +45,7 @@ public class RendererMixin {
 
 
     @Inject(method = "render", at = @At("HEAD"))
-    private void mainRender(ItemStack stack, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, CallbackInfo ci) {
+    private void mainRender(ItemStack stack, ModelTransformation.Mode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, CallbackInfo ci) {
         if(FabricLoader.getInstance().isDevelopmentEnvironment()) {
             if (stack.isOf(FabricShieldLib.fabric_shield)) {
                 FabricShieldItem.renderBanner(stack, matrices, vertexConsumers, light, overlay, modelFabricShield, FABRIC_SHIELD_BASE, FABRIC_SHIELD_BASE_NO_PATTERN);
