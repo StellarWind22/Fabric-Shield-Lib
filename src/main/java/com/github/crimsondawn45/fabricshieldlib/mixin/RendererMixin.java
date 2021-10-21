9ccfb68c817db2a6efbf3ffc90d9e81afeabe2a8
CrimsonDawn45 <gannonburks45@gmail.com>
2021-10-20 23:09:35 -0500

    Fixed item models

--- src/main/java/com/github/crimsondawn45/fabricshieldlib/mixin/RendererMixin.java
+++ src/main/java/com/github/crimsondawn45/fabricshieldlib/mixin/RendererMixin.java
@@ -22,7 +22,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@SuppressWarnings("deprecation")
 @Mixin (BuiltinModelItemRenderer.class)
 public class RendererMixin {
 
