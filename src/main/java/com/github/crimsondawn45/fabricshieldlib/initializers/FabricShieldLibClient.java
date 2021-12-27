09b4582378426d992b26285c896e8a677d9858b8
CringeStar <gozzook@gmail.com>
2021-12-27 12:57:11 -0800

    removed apparently unnecessary client init code

--- src/main/java/com/github/crimsondawn45/fabricshieldlib/initializers/FabricShieldLibClient.java
+++ src/main/java/com/github/crimsondawn45/fabricshieldlib/initializers/FabricShieldLibClient.java
@@ -3,13 +3,11 @@ package com.github.crimsondawn45.fabricshieldlib.initializers;
 import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricBannerShieldItem;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.entity.BannerBlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.entity.model.ShieldEntityModel;
 import net.minecraft.client.render.item.ItemRenderer;
-import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.texture.TextureCache;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
@@ -23,20 +21,6 @@ public class FabricShieldLibClient implements ClientModInitializer {
     
     @Override
     public void onInitializeClient() {
-        if(FabricLoader.getInstance().isDevelopmentEnvironment()) {
-
-            //Warn about dev code
-            FabricShieldLib.logger.warn("FABRIC SHIELD LIB DEVELOPMENT CODE RAN!!!, if you are not in a development environment this is very bad! Client side banner code ran!");
-
-            /*
-             * Registers sprite directories and model layer, will be done by player, dev code
-             */
-            //EntityModelLayerRegistry.registerModelLayer(fabric_banner_shield_model_layer, ShieldEntityModel::getTexturedModelData);
-            ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEX).register((atlasTexture, registry) -> {
-                registry.register(new Identifier(FabricShieldLib.MOD_ID, "entity/fabric_banner_shield_base"));
-                registry.register(new Identifier(FabricShieldLib.MOD_ID, "entity/fabric_banner_shield_base_nopattern"));
-            });
-        }
     }
 
     private static void renderEnchantmentGlint(Runnable runnable) {
@@ -53,25 +37,19 @@ public class FabricShieldLibClient implements ClientModInitializer {
     public static void renderBanner(ItemStack stack, Identifier nopattern_texture, Identifier banner_shield_texture){
         final ShieldEntityModel model = new ShieldEntityModel();
         final BannerBlockEntity renderBanner = new BannerBlockEntity();
-        final TextureCache.Manager banner_texture = new TextureCache.Manager("fabric_shield_", banner_shield_texture, "textures/entity/shield/");
+        final TextureCache.Manager banner_texture = new TextureCache.Manager(banner_shield_texture.toString(), banner_shield_texture, "textures/entity/shield/");
         if (stack.getSubTag("BlockEntityTag") != null) {
             renderBanner.readFrom(stack, FabricBannerShieldItem.getColor(stack));
             MinecraftClient.getInstance().getTextureManager().bindTexture(banner_texture.get(renderBanner.getPatternCacheKey(), renderBanner.getPatterns(), renderBanner.getPatternColors()));
         } else {
             MinecraftClient.getInstance().getTextureManager().bindTexture(nopattern_texture);
         }
-
         GlStateManager.pushMatrix();
         GlStateManager.scalef(1.0F, -1.0F, -1.0F);
         model.renderItem();
         if (stack.hasEnchantmentGlint()) {
-
             renderEnchantmentGlint(model::renderItem);
-
         }
-
         GlStateManager.popMatrix();
-
-
     }
 }
