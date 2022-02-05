4dd20869da8670198afa70de1bf4763619eb24f9
CrimsonDawn45 <gannonburks45@gmail.com>
2022-02-05 04:29:34 -0600

    Fixed some misc things

--- src/main/java/com/github/crimsondawn45/fabricshieldlib/lib/config/FabricShieldLibConfig.java
+++ src/main/java/com/github/crimsondawn45/fabricshieldlib/lib/config/FabricShieldLibConfig.java
@@ -8,6 +8,7 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 @Config.Gui.Background("minecraft:textures/block/dirt.png")
 public class FabricShieldLibConfig implements ConfigData {
     
+    public boolean enable_tooltips = true;
     public boolean allow_vanilla_shield_enchanting = true;
     @Comment("15 = Wood || 5 = Stone || 14 = Iron || 10 = Diamond || 22 = Gold")
     public int vanilla_shield_enchantability = 14;
