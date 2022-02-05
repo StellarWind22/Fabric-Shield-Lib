940e6588aa7957acff1692c2ccdbadda54b16d8a
CrimsonDawn45 <gannonburks45@gmail.com>
2022-02-04 20:08:52 -0600

    Altered ench target incase mods extend ShieldItem

--- src/main/java/com/github/crimsondawn45/fabricshieldlib/lib/enchantment/ShieldTarget.java
+++ src/main/java/com/github/crimsondawn45/fabricshieldlib/lib/enchantment/ShieldTarget.java
@@ -4,12 +4,12 @@ import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShield;
 import com.github.crimsondawn45.fabricshieldlib.mixin.EnchantmentTargetMixin;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.ShieldItem;
+import net.minecraft.item.Items;
 
 public class ShieldTarget extends EnchantmentTargetMixin {
 
     @Override
     public boolean isAcceptableItem(Item item) {
-        return item instanceof FabricShield || item instanceof ShieldItem;
+        return item instanceof FabricShield || item.equals(Items.SHIELD);
     }
 }
