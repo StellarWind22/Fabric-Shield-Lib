10011eb07d2f12eb840f905a8367b6d7fa6eec8b
CringeStar <gozzook@gmail.com>
2022-12-08 17:34:29 -0800

    Split Inject in LivingEntityMixin to fix the block event not being invoked

--- src/main/java/com/github/crimsondawn45/fabricshieldlib/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/crimsondawn45/fabricshieldlib/mixin/LivingEntityMixin.java
@@ -5,6 +5,7 @@ import com.github.crimsondawn45.fabricshieldlib.lib.event.ShieldBlockCallback;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -19,23 +20,23 @@ import net.minecraft.item.ItemStack;
  */
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-    
+
+
+	@Inject(at = @At(value = "HEAD"), method = "damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", locals = LocalCapture.CAPTURE_FAILHARD, cancellable = false)
+	private void invokeEvent(DamageSource source, float amount, CallbackInfoReturnable<Boolean> callbackInfo) {
+		//Handle shield blocking
+		LivingEntity entity = (LivingEntity)(Object)this;
+		ItemStack activeItem = entity.getActiveItem();
+		ShieldBlockCallback.EVENT.invoker().block(entity, source, amount, entity.getActiveHand(), activeItem);
+	}
+
     @Inject(at = @At(value = "TAIL"), method = "damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", locals = LocalCapture.CAPTURE_FAILHARD, cancellable = false)
 	private void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> callbackInfo) {
 		LivingEntity entity = (LivingEntity)(Object)this;
-		ItemStack activeItem = entity.getActiveItem();
-		
 		if(!entity.isInvulnerableTo(source) || !entity.world.isClient || !entity.isDead() || !(source.isFire() && entity.hasStatusEffect(StatusEffects.FIRE_RESISTANCE))) {
-			float g = 0.0F;
 			if (amount > 0.0F && ((LivingEntityAccessor)entity).fabricshieldlib$invokeBlockedByShield(source)) {
-
-				//Handle shield blocking
-                  ShieldBlockCallback.EVENT.invoker().block(entity, source, amount, entity.getActiveHand(), activeItem);
-    
 				//Handle Shield
 				((LivingEntityAccessor)entity).fabricshieldlib$invokeDamageShield(amount);
-				g = amount;
-				amount = 0.0F;
 				if (!source.isProjectile()) {
 					Entity sourceEntity = source.getSource();
 					
