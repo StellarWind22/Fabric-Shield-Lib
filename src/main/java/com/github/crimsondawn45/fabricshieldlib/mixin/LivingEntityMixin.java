7ef157653e52800975da80df4a4bff1eccd6cd2c
rikka0w0 <929513338@qq.com>
2024-12-05 02:14:47 +1100

    Remove a unnecessary mixin

--- src/main/java/com/github/crimsondawn45/fabricshieldlib/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/crimsondawn45/fabricshieldlib/mixin/LivingEntityMixin.java
@@ -4,18 +4,14 @@ import com.github.crimsondawn45.fabricshieldlib.lib.event.ShieldBlockCallback;
 import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShieldUtils;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.registry.tag.DamageTypeTags;
 import net.minecraft.server.world.ServerWorld;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -26,12 +22,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @SuppressWarnings("ALL")
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-	/**
-	 * @param attacker entity attacking the entity blocking.
-	 */
-	@Shadow
-	protected abstract void takeShieldHit(LivingEntity attacker);
-
 	@Inject(
 			at = @At(value = "HEAD"),
 			method = "damage(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/damage/DamageSource;F)Z"
@@ -39,30 +29,9 @@ public abstract class LivingEntityMixin {
 	private void invokeEvent(ServerWorld world, DamageSource source, float amount,
 			CallbackInfoReturnable<Boolean> callbackInfo) {
 		// Handle shield blocking
-        LivingEntity entity = (LivingEntity) (Object) this;
-        ItemStack activeItem = entity.getActiveItem();
-        ShieldBlockCallback.EVENT.invoker().block(entity, source, amount, entity.getActiveHand(), activeItem);
-	}
-
-	@Inject(
-			at = @At(value = "TAIL"),
-			method = "damage(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/damage/DamageSource;F)Z"
-		)
-	private void damage(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> callbackInfo) {
-        LivingEntity entity = (LivingEntity) (Object) this;
-        if (!entity.isInvulnerableTo(world, source) || !entity.getWorld().isClient || !entity.isDead() || !(source.isIn(DamageTypeTags.IS_FIRE) && entity.hasStatusEffect(StatusEffects.FIRE_RESISTANCE))) {
-            if (amount > 0.0F && entity.blockedByShield(source)) {
-                // Handle Shield
-            	entity.damageShield(amount);
-                if (!source.isIn(DamageTypeTags.IS_PROJECTILE)) {
-                    Entity sourceEntity = source.getSource();
-
-                    if (sourceEntity instanceof LivingEntity) {
-                        this.takeShieldHit((LivingEntity) sourceEntity);
-                    }
-                }
-            }
-        }
+		LivingEntity entity = (LivingEntity) (Object) this;
+		ItemStack activeItem = entity.getActiveItem();
+		ShieldBlockCallback.EVENT.invoker().block(entity, source, amount, entity.getActiveHand(), activeItem);
 	}
 
 	/**
