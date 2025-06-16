04bc3ed1211b44e2f5d6895720ddcdd4e15078cd
Michael Arakelyan <gozzook@gmail.com>
2025-06-15 23:44:05 -0700

    opening config menu crashes game, but maybe i shouldntve sloppily removed it from the project

--- src/main/java/com/github/crimsondawn45/fabricshieldlib/lib/config/FabricShieldLibConfig.java
+++ src/main/java/com/github/crimsondawn45/fabricshieldlib/lib/config/FabricShieldLibConfig.java
@@ -1,24 +1,24 @@
-//package com.github.crimsondawn45.fabricshieldlib.lib.config;
-//
-//import eu.midnightdust.lib.config.MidnightConfig;
-//
-//public class FabricShieldLibConfig extends MidnightConfig {
-//
-//    @Entry
-//    public static boolean enable_tooltips = true;
-//
-//    @Comment
-//    public static Comment enchantability_convention;
-//
-//    @Entry
-//    public static int vanilla_shield_enchantability = 14;
-//
-//    @Entry
-//    public static boolean universal_disable = false;
-//
-//    @Comment
-//    public static Comment universal_disable_description_1;
-//
-//    @Comment
-//    public static Comment universal_disable_description_2;
-//}
+package com.github.crimsondawn45.fabricshieldlib.lib.config;
+
+import eu.midnightdust.lib.config.MidnightConfig;
+
+public class FabricShieldLibConfig extends MidnightConfig {
+
+    @Entry
+    public static boolean enable_tooltips = true;
+
+    @Comment
+    public static Comment enchantability_convention;
+
+    @Entry
+    public static int vanilla_shield_enchantability = 14;
+
+    @Entry
+    public static boolean universal_disable = false;
+
+    @Comment
+    public static Comment universal_disable_description_1;
+
+    @Comment
+    public static Comment universal_disable_description_2;
+}
