c071af4daeb8491460487118d79d1ebacfec0b44
Michael Arakelyan <gozzook@gmail.com>
2025-06-17 11:08:18 -0700

    final changes for 1.21.6 port

--- src/main/java/com/github/crimsondawn45/fabricshieldlib/lib/config/FabricShieldLibModMenuIntegration.java
+++ src/main/java/com/github/crimsondawn45/fabricshieldlib/lib/config/FabricShieldLibModMenuIntegration.java
@@ -1,17 +1,17 @@
-//package com.github.crimsondawn45.fabricshieldlib.lib.config;
-//
-//import com.github.crimsondawn45.fabricshieldlib.initializers.FabricShieldLib;
-//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-//import com.terraformersmc.modmenu.api.ModMenuApi;
-//import eu.midnightdust.lib.config.MidnightConfig;
-//import net.fabricmc.api.EnvType;
-//import net.fabricmc.api.Environment;
-//
-//@Environment(EnvType.CLIENT)
-//public class FabricShieldLibModMenuIntegration implements ModMenuApi {
-//
-//    @Override
-//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//        return parent -> MidnightConfig.getScreen(parent, FabricShieldLib.MOD_ID);
-//    }
-//}
+package com.github.crimsondawn45.fabricshieldlib.lib.config;
+
+import com.github.crimsondawn45.fabricshieldlib.initializers.FabricShieldLib;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import eu.midnightdust.lib.config.MidnightConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class FabricShieldLibModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> MidnightConfig.getScreen(parent, FabricShieldLib.MOD_ID);
+    }
+}
